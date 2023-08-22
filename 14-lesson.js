dd6ab0790dfc9fa551886fc5c44c332fc6258441
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-08-22 12:32:34 +0300

    add documents

--- 14-lesson.js
+++ 14-lesson.js
@@ -82,18 +82,18 @@ console.log(car_3.brand)
 
 
 const promise = new Promise((res) => {
-    console.log(1);
-    setTimeout(()=>{
-      console.log(4)
-      res()
-      console.log(5)
-    },1)
-  })
-  console.log(2)
-  promise.then(() =>{console.log(3)})
-  Promise.resolve().then(()=>{console.log(6)})
+  console.log(1);
   setTimeout(()=>{
-    console.log(100)
-  },0)
-  
+    console.log(4)
+    res()
+    console.log(5)
+  },1)
+})
+console.log(2)
+promise.then(() =>{console.log(3)})
+Promise.resolve().then(()=>{console.log(6)})
+setTimeout(()=>{
+  console.log(100)
+},0)
+
 
